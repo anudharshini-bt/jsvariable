72c509477d49700803a429bbd307d41d35226815
anudharshini-bt <anudharshini1104@gmail.com>
2024-07-07 20:44:11 +0530

    first commit

---
+++ prg5.js
@@ -0,0 +1,21 @@
+
+var message;
+
+message = "Hello World!";
+console.log(message);
+
+message = 'Congratulations! Your tweet has won a prize...';
+console.log(message);
+
+message = "In spoken English"
+console.log(message);
+
+message_1 = "hello"
+message_2 = "world!"
+message = "hello"+"world"
+console.log(message);
+
+sentence_1 = 'congratulations!'
+sentence_2 = 'Your tweet has won a prize...'
+sentence_3 = 'Congratulations!'+'Your tweet has won a prize...'+'In spoken English'
+console.log(message);
---
+++ prg6.js
@@ -0,0 +1,9 @@
+playerName = "Lionel Messi";
+locationName = "Argentina";
+
+console.log(playerName + " is in score of 200 " +locationName);
+
+
+var score;
+score = 200;
+console.log(score);
